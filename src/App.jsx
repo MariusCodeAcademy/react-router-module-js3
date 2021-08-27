2f4f6ad59eff31c56174ff96ae445e71296d0770
Marius Kuzminskas <marius.kuzminskas@ca.com>
2021-08-27 10:50:48 +0300

    nested comments dynamic route

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import AllQuotes from './pages/AllQuotes';
 import NewQuote from './pages/NewQuote';
 import QuoteDetails from './pages/QuoteDetails';
 function App() {
   return (
     <Switch>
+      <Route path='/' exact>
+        <Redirect to='/quotes' />
+      </Route>
       <Route path='/quotes' exact>
         <AllQuotes />
       </Route>
@@ -21,6 +24,11 @@ function App() {
 export default App;
 
 /*
+domain.com/ peradresuoti i quotes psl
+
+QuoteDetails psl aspausdinti quoteId
+
+
 domain.com/quotes 
 domain.com/quotes/:quoteId 
 domain.com/new-quote
--- src/pages/QuoteDetails.jsx
+++ src/pages/QuoteDetails.jsx
@@ -1,5 +1,17 @@
+import { useParams, Route } from 'react-router-dom';
 const QuoteDetails = () => {
-  return <h2>QuoteDetails page</h2>;
+  const { quoteId } = useParams();
+  console.log(quoteId);
+  return (
+    <>
+      <h2>QuoteDetails page</h2>
+      <p>Quote id: {quoteId}</p>
+      <Route path={`/quotes/${quoteId}/comments`}>
+        <h3>Commments test</h3>
+      </Route>
+    </>
+  );
 };
 
 export default QuoteDetails;
+// sukurti toki route su dinamine dalimi http://localhost:3000/quotes/some-quote/comment
