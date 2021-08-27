3dbefc58a2bdf0b8eb64908c1b72e9a49560d673
Marius Kuzminskas <marius.kuzminskas@ca.com>
2021-08-27 09:52:32 +0300

    routes in place

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
+import { Route, Switch } from 'react-router-dom';
 function App() {
   return (
-    <div>
-
-    </div>
+    <Switch>
+      <Route path='/quotes' exact>
+        <h2>all q</h2>
+      </Route>
+      <Route path='/quotes/:quoteId'>
+        <h2>q details</h2>
+      </Route>
+      <Route path='/new-quote'>
+        <h2>new q</h2>
+      </Route>
+    </Switch>
   );
 }
 
 export default App;
+
+/*
+domain.com/quotes 
+domain.com/quotes/:quoteId 
+domain.com/new-quote
+
+Sukurti puslapiu komponentus(pages)
+AllQuotes, NewQuote, QuoteDetails 
+*/
